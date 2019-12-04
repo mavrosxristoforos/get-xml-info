8450cf68856e489be95e1792f4a14d8ab436720d
Christopher Mavros <mavrosxristoforos@gmail.com>
2019-12-04 12:46:14 +0200

    Implement action

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # get-xml-info
  Get Information from XML files to use into your GitHub workflows
+
+## Inputs
+
+### `xml-file`
+
+**Required** The path of the XML file to parse. Default `"test.xml"`.
+
+### `xpath`
+
+**Required** The xpath of the nodes from which you want to retrieve information. Default `"//element"`.
+
+## Example usage
+
+    uses: mavrosxristoforos/get-xml-info@1.0
+      with:
+        xml-file: 'yourfile.xml'
+        xpath: '//element'
+
+## Console usage
+
+    node index.js -f path/to/xmlfile -p //element
+
+## Outputs
+
+### `info`
+
+The content of the matched nodes. If your XPath matches more than one nodes, the output is an array.
--- index.js
+++ index.js
@@ -6,9 +6,37 @@ try {
     
   var argv = require('minimist')(process.argv.slice(2));
   var xmlFile = (typeof argv.f !== 'undefined') ? argv.f : process.env.GITHUB_WORKSPACE+'/'+core.getInput('xml-file', { required: true });
+  var xpathToSearch = (typeof argv.p !== 'undefined') ? argv.p : core.getInput('xpath', { required: true });
 
-  console.log(xmlFile);
+  console.log(`File to read: ${xmlFile}`);
+  console.log(`XPath: ${xpathToSearch}`);
 
+  var xpath = require('xpath'), dom = require('xmldom').DOMParser
+ 
+  fs.readFile(xmlFile, 'utf8', function read(err, data) {
+    if (err) {
+      core.setFailed(err.message);
+    }
+    else {
+      console.log('File was read successfully. Proceeding to parse DOM.');
+      
+      var doc = new dom().parseFromString(data);
+      var nodes = xpath.select(xpathToSearch, doc);
+
+      console.log(`Found ${nodes.length} nodes.`);
+
+      if (nodes.length) {
+        var output = [];
+        for (var i = 0; i < nodes.length; i++) {
+          output.push(nodes[i].firstChild.data);
+        }
+        core.setOutput('info', output);
+      }
+      else {
+        core.setFailed('Your xpath did not return any nodes.');
+      }
+    }
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
